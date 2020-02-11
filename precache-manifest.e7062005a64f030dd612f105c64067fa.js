f939065d9d0f5770fc31b22f4b0ef1b12b232b71
nago3579 <nago3579@colorado.edu>
2020-02-11 13:33:35 -0700

    Updates

---
+++ precache-manifest.e7062005a64f030dd612f105c64067fa.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "10b4c7eebdeea92cb19b",
+    "url": "/bandFanPage/static/js/app.843d76f9.chunk.js"
+  },
+  {
+    "revision": "e6f317ddfcf396943bed",
+    "url": "/bandFanPage/static/js/runtime~app.73f5b120.js"
+  },
+  {
+    "revision": "a2ef4efe92a60820da20",
+    "url": "/bandFanPage/static/js/2.c6f28c66.chunk.js"
+  },
+  {
+    "revision": "ad044508b8122a22cea021f15ebd5c21",
+    "url": "/bandFanPage/static/media/wildermiss1.ad044508.jpg"
+  },
+  {
+    "revision": "da9680833641197040ad6f8ca18c578d",
+    "url": "/bandFanPage/static/media/wildermiss2.da968083.jpg"
+  },
+  {
+    "revision": "28560b91e61c4156be78e9ade3c9e344",
+    "url": "/bandFanPage/static/media/wildermiss3.28560b91.jpg"
+  },
+  {
+    "revision": "00d5a5cd10522ec9ae867816b77a1c18",
+    "url": "/bandFanPage/static/media/wildermiss4.00d5a5cd.jpg"
+  },
+  {
+    "revision": "8387108b58a041a4ebd8d06e2a88555e",
+    "url": "/bandFanPage/static/media/wildermiss5.8387108b.png"
+  },
+  {
+    "revision": "64369dec4ca84762088188e611add7d6",
+    "url": "/bandFanPage/static/media/wildermiss6.64369dec.png"
+  },
+  {
+    "revision": "37384ef7f0996b9fbffaedef4eae0aba",
+    "url": "/bandFanPage/static/media/wildermiss7.37384ef7.jpg"
+  },
+  {
+    "revision": "fd4d204c6ecea0cb5e66c2142d4a134b",
+    "url": "/bandFanPage/static/media/wildermiss8.fd4d204c.jpg"
+  },
+  {
+    "revision": "9db21401c13e7145f913fedead9bf2e7",
+    "url": "/bandFanPage/static/media/wildermiss9.9db21401.jpg"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/bandFanPage/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/bandFanPage/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/bandFanPage/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/bandFanPage/expo-service-worker.js"
+  },
+  {
+    "revision": "7be06ca65ef9fa332d4f2aafb5f889c1",
+    "url": "/bandFanPage/manifest.json"
+  },
+  {
+    "revision": "e741ccf1172af3c5c3244dacdffd322c",
+    "url": "/bandFanPage/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "dcaeb12f332621f575c6538b03d13081",
+    "url": "/bandFanPage/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "edf6ff686aa6eb1f2ae7b74797a2e822",
+    "url": "/bandFanPage/index.html"
+  },
+  {
+    "revision": "8678e8806e68101cd5183a3d7a6f4187",
+    "url": "/bandFanPage/static/js/app.843d76f9.chunk.js.gz"
+  },
+  {
+    "revision": "4745152052602cf35818e09bb546b08d",
+    "url": "/bandFanPage/static/js/runtime~app.73f5b120.js.gz"
+  },
+  {
+    "revision": "4a2a8019995a0671e46de9607f5dfe6c",
+    "url": "/bandFanPage/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "3bfea9630d600cc25fbc7226d86c291a",
+    "url": "/bandFanPage/static/js/2.c6f28c66.chunk.js.gz"
+  }
+];
